5840145495107bbf71ebe7d53926463c442a9b27
Guangchuang Yu <guangchuangyu@gmail.com>
2020-04-01 09:56:06 +0800

    update as-raster method

--- R/as-raster.R
+++ R/as-raster.R
@@ -2,8 +2,13 @@
 ##' @method as.raster sticker
 ##' @export
 as.raster.sticker <- function(x,  ...) {
-    filename <- tempfile(fileext = ".png")
-    save_sticker(filename, x, ...)
+    filename <- x$plot_env$filename
+    if (is.null(filename) || is.na(filename) || !file.exists(filename)) {
+        filename <- tempfile(fileext = ".png")
+        class(x) <- c("gg", "ggplot")
+        save_sticker(filename, x, ...)
+    }
+
     as.raster(magick::image_read(filename))
 }
 
@@ -12,3 +17,4 @@ as.raster.sticker <- function(x,  ...) {
 plot.sticker <- function(x, ...) {
     plot(as.raster.sticker(x), ...)
 }
+
